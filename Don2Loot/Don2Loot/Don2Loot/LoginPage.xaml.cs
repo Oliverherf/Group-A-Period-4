695d9b0ded4ac37ef3c81b5243d3c30be7d4da66
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-05-30 10:50:36 +0200

    Login Page

--- Don2Loot/Don2Loot/Don2Loot/LoginPage.xaml.cs
+++ Don2Loot/Don2Loot/Don2Loot/LoginPage.xaml.cs
@@ -16,5 +16,30 @@ namespace Don2Loot
         {
             InitializeComponent();
         }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            if (txtUserName.Text.Length >= 20)
+            {
+                //DisplayAlert("Alert", txtUserName.MaxLength.ToString(), "ok");
+                DisplayAlert("Alert!", "Username is too long", "Use a Grammar Check!");
+            }
+            /*else
+            {
+                DisplayAlert("Alert", txtUserName.Text.Length.ToString(), "ok");
+            }*/
+        }
+        static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
