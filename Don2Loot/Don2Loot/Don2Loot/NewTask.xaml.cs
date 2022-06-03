ed487e42f7cd961db2c3f07724734f533ed58dfc
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-06-03 11:47:04 +0200

    Save and Cancel Buttons

--- Don2Loot/Don2Loot/Don2Loot/NewTask.xaml.cs
+++ Don2Loot/Don2Loot/Don2Loot/NewTask.xaml.cs
@@ -1,26 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Don2Loot
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewTask : ContentPage
     {
+           
         public NewTask()
         {
             InitializeComponent();
-           
+            this.BindingContext = this;
+            NavigationPage.SetHasNavigationBar(this, true);
+            this.Title = "XamFile Writer";
         }
+        private void Cancel(object sender, EventArgs e)
+        {
+            txtFileText.Text = "";
+            txtFileName.Text = "";
 
+        }
+        protected async void Save(object sender, EventArgs e)
+        {
+            Task task = new Task();
+            task.TaskDescription = txtFileText.Text;
+            task.TaskName = txtFileName.Text;
+            App.Database.saveTask(task);
+            List<Task> task1 = new List<Task>();
+            task1 = await App.Database.getTask();
+            DisplayAlert("Congrats!", "Note is saved!", "Go Back");
+            return;
+        }
         private void backButton(object sender, EventArgs e)
         {
-            Navigation.PopAsync();
+            Navigation.PushAsync(new MainPage());
         }
     }
 }
