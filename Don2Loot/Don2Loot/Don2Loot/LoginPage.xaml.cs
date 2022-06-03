60af3095e6f0ca611c03a1d91506c1c57179ebe3
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-06-03 12:06:01 +0200

    Log In Page

--- Don2Loot/Don2Loot/Don2Loot/LoginPage.xaml.cs
+++ Don2Loot/Don2Loot/Don2Loot/LoginPage.xaml.cs
@@ -18,62 +18,67 @@ namespace Don2Loot
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            if(string.IsNullOrEmpty(txtUserName.Text))
-            {
-                DisplayAlert("Alert!", "Username must contain 2-20 characters", "Use a Grammar Check!");
-                return;
-            }
-            
+                if (string.IsNullOrEmpty(txtUserName.Text))
+                {
+                    DisplayAlert("Alert!", "All fields shoud be fill in!", "Go back");
+                    return;
+                }
+                if (string.IsNullOrEmpty(txtEmail.Text))
+                {
+                    DisplayAlert("Alert!", "All fields shoud be fill in!", "Go back");
+                    return;
+                }
+
             if (txtUserName.Text.Length > 20 && txtUserName.Text.Length < 2)
-            {
-                //DisplayAlert("Alert", txtUserName.MaxLength.ToString(), "ok");
-                DisplayAlert("Alert!", "Username must contain 2-20 characters", "Use a Grammar Check!");
-                return;
-            }
+                {
+                    //DisplayAlert("Alert", txtUserName.MaxLength.ToString(), "ok");
+                    DisplayAlert("Alert!", "Username must contain 2-20 characters", "Use a Grammar Check!");
+                    return;
+                }
 
-            if (!IsAllLetters(txtUserName.Text))
-            {
-                DisplayAlert("Alert!", "Username should contain only letters", "Use a Grammar Check!");
-                return;
-            }
+                if (!IsAllLetters(txtUserName.Text))
+                {
+                    DisplayAlert("Alert!", "Username should contain only letters", "Use a Grammar Check!");
+                    return;
+                }
 
-            if (!IsValidEmail(txtEmail.Text))
-            {
-                DisplayAlert("Alert!", "Email is not valid", "Use a Grammar Check!");
-                return;
+                if (!IsValidEmail(txtEmail.Text))
+                {
+                    DisplayAlert("Alert!", "Email is not valid", "Use a Grammar Check!");
+                    return;
+                }
+                //System.Diagnostics.Debug.WriteLine("Success");
+                var contact = new Contact
+                {
+                    Email = txtEmail.Text,
+                    Name = txtUserName.Text.ToUpper()
+                };
+                var mainPage = new MainPage();
+                mainPage.BindingContext = contact;
+                Navigation.InsertPageBefore(mainPage, this);    //inserting page before current page
+                await Navigation.PopAsync();                    //popping current page off the navigation stack (Mainpage is now the main page/root page
             }
-            //System.Diagnostics.Debug.WriteLine("Success");
-            var contact = new Contact
+            public bool IsAllLetters(string s)
             {
-                Email = txtEmail.Text,
-                Name = txtUserName.Text.ToUpper()
-            };
-            var mainPage = new MainPage();
-            mainPage.BindingContext = contact;
-            Navigation.InsertPageBefore(mainPage, this);    //inserting page before current page
-            await Navigation.PopAsync();                    //popping current page off the navigation stack (Mainpage is now the main page/root page
-        }
-        public bool IsAllLetters(string s)
-        {
-            foreach (char c in s)
+                foreach (char c in s)
+                {
+                    if (!Char.IsLetter(c))
+                        return false;
+                }
+                return true;
+            }
+            public bool IsValidEmail(string email)
             {
-                if (!Char.IsLetter(c))
+                try
+                {
+                    var addr = new System.Net.Mail.MailAddress(email);
+                    return addr.Address == email;
+                }
+                catch
+                {
                     return false;
+                }
             }
-            return true;
-        }
-        public bool IsValidEmail(string email)
-        {
-            try
-            {
-                var addr = new System.Net.Mail.MailAddress(email);
-                return addr.Address == email;
-            }
-            catch
-            {
-                return false;
-            }
-        }
     }
 
     internal class Contact
