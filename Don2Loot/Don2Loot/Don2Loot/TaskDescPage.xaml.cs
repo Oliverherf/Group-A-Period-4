22f5a533ff618dd5ea705d923fb39c18bd782625
Mathew Shardin <mathew.shardin@student.nhlstenden.com>
2022-06-15 12:20:59 +0200

    Initial commit for Task Page

---
+++ Don2Loot/Don2Loot/Don2Loot/TaskDescPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Don2Loot {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TaskDescPage : ContentPage {
+        public TaskDescPage() {
+            InitializeComponent();
+        }
+
+        //Make sure coins are updated when page is opened
+        protected override async void OnAppearing() {
+            base.OnAppearing();
+            List<User> users = new List<User>();
+            users = await App.Database.getUser();
+            taskCoins.Text = users[0].UserCoins.ToString();
+        }
+    }
+}
